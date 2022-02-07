6cf0bf19f21e961ab8a9d9f14fcd3ea1812bc4e0
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-07 10:12:29 -0500

    commit tree 92cfac17d88361e66ce73647c4097eb8b25a25c5

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your functions here
+// my own map-like methods
+    //map returns an array with all values made negative
+    //map returns an array with the original values
+    //map returns an array with the original values multiplied by 2
+    //map returns an array with the original values squared
+function map(array, func) {
+   let newArray = []
+   for (let i = 0; i < array.length; i++) {
+        let element = array[i]
+        newArray.push(func(element))
+    }   
+    return newArray;
+}
+
+//my own reduce-like methods
+    //reduce returns a running total when not given a starting point
+    //reduce returns a running total when given a starting point
+    //reduce returns true when all values are true
+    //reduce returns false when any value is false
+    //reduce returns true when a true value is present
+    //reduce returns false when no true value is present
+
+function reduce(array, func, startingValue) {
+    let r = !!startingValue ? startingValue : array[0];
+    let i = !!startingValue ? 0 : 1;
+    
+    for (; i < array.length; i++) {
+        r = func(array[i], r);
+    }
+    return r;
+}
+
+
